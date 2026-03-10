8be5524e8e16ee323df2b9343468eee2180b507a
bromolima <bromolima@gmail.com>
2026-03-10 06:21:47 -0300

    feat: dependency injection setup in main

--- server/go.mod
+++ server/go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/gin-gonic/gin v1.12.0
 	github.com/google/uuid v1.6.0
 	github.com/joho/godotenv v1.5.1
+	golang.org/x/crypto v0.48.0
 )
 
 require (
@@ -24,6 +25,7 @@ require (
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.3.0 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
+	github.com/lib/pq v1.11.2 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
@@ -34,7 +36,6 @@ require (
 	github.com/ugorji/go/codec v1.3.1 // indirect
 	go.mongodb.org/mongo-driver/v2 v2.5.0 // indirect
 	golang.org/x/arch v0.22.0 // indirect
-	golang.org/x/crypto v0.48.0 // indirect
 	golang.org/x/net v0.51.0 // indirect
 	golang.org/x/sys v0.41.0 // indirect
 	golang.org/x/text v0.34.0 // indirect
--- server/main.go
+++ server/main.go
@@ -1,10 +1,19 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
+	"github.com/gin-gonic/gin"
 	"github.com/m-bromo/my-game-list/config"
+	"github.com/m-bromo/my-game-list/internal/infra/database"
+	"github.com/m-bromo/my-game-list/internal/infra/database/sqlc"
+	"github.com/m-bromo/my-game-list/internal/repository"
+	"github.com/m-bromo/my-game-list/internal/service"
+	"github.com/m-bromo/my-game-list/internal/web/handler"
+	"github.com/m-bromo/my-game-list/internal/web/routes"
 	"github.com/m-bromo/my-game-list/pkg/logging"
+	"github.com/m-bromo/my-game-list/pkg/security"
 )
 
 func main() {
@@ -13,7 +22,30 @@ func main() {
 		log.Fatal(err)
 	}
 
-	log := logging.NewLogger(config)
+	logger := logging.NewLogger(config)
+	db, err := database.NewPostgresConnection(config)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	log.Log.Info("starting application")
+	g := gin.New()
+
+	querier := sqlc.New(db)
+
+	userRepository := repository.NewSqlcUserRepository(querier)
+	gameRepository := repository.NewSqlcGameRepository(querier)
+
+	hasher := security.NewHasher()
+
+	authService := service.NewUserService(userRepository, hasher)
+	gameService := service.NewGameService(gameRepository)
+
+	authHandler := handler.NewAuthHandler(authService)
+	gameHandler := handler.NewGameHandler(gameService)
+
+	routes.SetupRoutes(g, authHandler, gameHandler)
+
+	log.Fatal(g.Run(fmt.Sprintf("%s:%s", config.Api.Host, config.Api.Port)))
+
+	logger.Log.Info("starting application")
 }
